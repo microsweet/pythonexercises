4b0ee3afea2aaf24cb9028725c6b7dfedef78596
microsweet <599058045@qq.com>
2019-09-27 17:14:46 +0800

    my python exercises

---
+++ 11/employee.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+class Employee():
+    def __init__(self, first_name, last_name, salary):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.salary = salary
+
+    def give_raise(self,salary=5000):
+        self.salary += salary
+
---
+++ 11/test_employee.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+import unittest
+from employee import Employee
+
+class test_empolyee(unittest.TestCase):
+    def setUp(self):
+        self.employee = Employee('Tony', 'Stark', 10000)
+
+    def test_give_default_raise(self):
+        self.employee.give_raise()
+        self.assertEqual(self.employee.salary, 15000)
+
+    def test_give_custom_raise(self):
+        self.employee.give_raise(1000)
+        self.assertEqual(self.employee.salary, 11000)
+
+unittest.main()
