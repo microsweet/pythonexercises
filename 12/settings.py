03d4d161d530f855515cb840966c3246f626ac7f
microsweet <weide.yang@hotmail.com>
2019-10-04 18:45:30 +0800

    my pythonexercises

---
+++ 12/settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 300
+        self.screen_height = 500
+        #self.bg_color = (9, 187, 180)
+        self.bg_color = (255, 255, 255)
