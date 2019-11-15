53a1991b1358d20f07f6e8b77ff8240f5105cbc1
microsweet <599058045@qq.com>
2019-11-15 17:13:12 +0800

    my python exercises

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    """TODO: Docstring for index.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context, request))
 
-    :request: TODO
-    :returns: TODO
-
-    """
-    return HttpResponse("Hello, world. You're at the polls index.")
